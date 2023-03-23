1f6e4098e0ec94fa8414f5350957946fb42c3f59
laura05010 <lcmmadrid5@gmail.com>
2023-03-23 18:34:11 -0400

    created clean data file

--- data_counts.py
+++ data_counts.py
@@ -6,57 +6,61 @@ file_path = "cleaned_merged_fairy_tales_without_eos.txt"
 def blank_lines():
     with open(file_path, "r") as f:
         lines = f.readlines()
-        empty = 0 
+        empty = 0
         for line in lines:
             # check if line is empty
             if line == '\n':
-                empty += 1 
+                empty += 1
 
     return empty
+
 
 def characters():
     with open(file_path, "r") as f:
         lines = f.readlines()
-        characters = 0 
+        characters = 0
         for line in lines:
-            characters += len(line) 
+            characters += len(line)
 
     return characters
-        
+
 
 def words():
     with open(file_path, "r") as f:
         lines = f.readlines()
-        words = 0 
+        words = 0
         for line in lines:
-            words += len(line.split()) 
+            words += len(line.split())
 
     return words
+
 
 def sentences():
     with open(file_path, "r") as f:
         lines = f.readlines()
-        sentences = 0 
+        sentences = 0
         for line in lines:
-            sentences += len(line.split(".")) 
+            sentences += len(line.split("."))
 
     return sentences
 
-# unique words 
+# unique words
+
+
 def unique_words():
     with open(file_path, "r") as f:
         lines = f.readlines()
-        unique_words = {} 
+        unique_words = {}
         for line in lines:
             for word in line.split():
                 unique_words[word] = unique_words.get(word, 0) + 1
-        
 
     return unique_words
 
+
 def count_punctuation():
     with open(file_path, "r") as f:
-        question_marks = 0 
+        question_marks = 0
         exclamation_marks = 0
         periods = 0
         commas = 0
